688af1f74e8ef7ceb6eb7b66e2857de016dd3114
Roger Abelló <roger.abellop@educem.net>
2025-04-01 16:20:12 +0200

    renamed spanish files for generic names

--- php/login.php
+++ php/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once('../connexioDB.php');
+require_once('../DBConnection.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $usuari = trim($_POST['usuari'] ?? '');
--- php/register.php
+++ php/register.php
@@ -1,5 +1,5 @@
 <?php
-require_once('../connexioDB.php');
+require_once('../DBConnection.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = trim($_POST['username']);
